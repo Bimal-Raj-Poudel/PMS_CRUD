207874782c870ad07975cd61c4946dab74562587
Bimal Raj Poudel <startertechz@gmail.com>
2024-04-04 13:33:24 +0545

    card is added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,23 +1,43 @@
 import React from "react";
-import './Home.css'
+import "./Home.css";
 import Navbar from "../component/navbar/Navbar";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProduct = async () => {
+    const response = await axios.get(
+      "https://660e43116ddfa2943b3635a7.mockapi.io/products"
+    );
+    setProducts(response.data);
+  };
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  console.log(products);
+
   return (
     <>
-    <Navbar/>
-      <div className="product-card">
-        <img
-          className="product-image"
-          src="product-image.jpg"
-          alt="Product Image"
-        />
-        <h2 className="product-name">Product Name</h2>
-        <p className="product-description">
-          This is a short description of the product.
-        </p>
-      </div>
+      <Navbar />
+
+      {products.map((product) => {
+        return (
+          <div key={product.id} className="product-card">
+            <img
+              className="product-image"
+              src={product.}
+              alt="Product Image"
+            />
+            <h2 className="product-name">{product.ProductName}</h2>
+            <p className="product-description">
+              {product.productDescription}
+            </p>
+          </div>
+        );
+      })}
     </>
   );
 };
